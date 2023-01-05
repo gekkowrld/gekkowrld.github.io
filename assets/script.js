1581f71b6bdf22e26960743b8158baa55880d7eb
gekkowrld <108436933+gekkowrld@users.noreply.github.com>
2023-01-05 20:59:56 +0000

    Add a "Welcome" section

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// This is for the menu icon (Should not be touched)
+// const navMenu = document.querySelector('#menu-nav-icon');
+// navMenu.addEventListener('click', toggleClass);
+// function toggleClass(){
+//     navMenu.classList.toggle('open');
+// }
