a93c1494961d2993530efa0f3fc5b333d35a31e8
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-01-11 01:57:47 +0300

    Split the pages and change the directories

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-// This is for the menu icon (Should not be touched)
-// const navMenu = document.querySelector('#menu-nav-icon');
-// navMenu.addEventListener('click', toggleClass);
-// function toggleClass(){
-//     navMenu.classList.toggle('open');
-// }
